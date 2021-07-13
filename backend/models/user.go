6140cfcba32381a70c0910f3fdba2db18dc57764
LeNgocPhuc99 <lengocphuc0099@gmail.com>
2021-07-13 15:37:27 +0700

    update user struct

--- backend/models/user.go
+++ backend/models/user.go
@@ -6,6 +6,4 @@ type User struct {
 	Email     string `json:"email" bson:"email"`
 	Username  string `json:"username" bson:"username"`
 	Password  string `json:"password" bson:"password"`
-	// CreateAt  time.Time `json:"createAt" bson:"createAt"`
-	// UpdatedAt time.Time `json:"updateAt" bson:"updateAt"`
 }
